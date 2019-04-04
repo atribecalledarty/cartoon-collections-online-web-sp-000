367b4a1e739c2def7a41f382a10cf8a7dbdf6adf
Arthur Song <arth3rs0ng@gmail.com>
2019-04-04 22:53:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,9 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  
+  array.any? do |name|
+    name.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
